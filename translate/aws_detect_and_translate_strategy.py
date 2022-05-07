f35fed9131e448a105902d3d3cf40bc6c20f1993
Cristian Danila <mdanila@cisco.com>
2022-05-08 02:58:56 +0300

    Add AWSDetectAndTranslateStrategy strategy

---
+++ translate/aws_detect_and_translate_strategy.py
@@ -0,0 +1,9 @@
+from translate.translate_startegy import TranslateStrategy
+
+
+class AWSDetectAndTranslateStrategy(TranslateStrategy):
+
+    def translate(self, target: str) -> str:
+        # detect and trasnlate
+
+        pass
