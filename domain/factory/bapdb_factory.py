2dc8b740ca6b491f250a4ccb451154a2a65538b7
Cristian Danila <mdanila@cisco.com>
2022-05-08 15:18:38 +0300

    Complete BAPDB factory implementation

--- domain/factory/bapdb_factory.py
+++ domain/factory/bapdb_factory.py
@@ -1,4 +1,9 @@
+import json
+
 from domain.BAPDBData import BAPDBData
+
+
+
 
 
 class BAPDBFactory:
@@ -7,12 +12,42 @@ class BAPDBFactory:
         pass
 
     @staticmethod
+    def _collect_diagnostics(diagnostic_json):
+        a = json.loads(diagnostic_json)
+
+        diagnostic = []
+
+        for e in a['Diagnosen']['Diagnosen']['List']:
+            title = e['Titel']
+
+            if "Inhalt" in e.keys():
+                content = e['Inhalt']
+                diagnostic.append((title, content))
+            else:
+                diagnostic.append((title, ''))
+
+        return diagnostic
+
+
+    @staticmethod
     def build(data):
-        berteleug = data['berteleug']
-        anamnesis = data['anamnesis']
-        procedure = data['procedure']
-        diagnostic = data['diagnostic']
-        bifunde = data['bifunde']
+        berteleug = data[0]
+        anamnesis = data[1]
+        procedure = data[2]
+
+        try:
+            diagnostic_json = json.loads(data[3])
+            diagnostic = BAPDBFactory._collect_diagnostics(diagnostic_json)
+        except Exception as e:
+            diagnostic = json.loads("{}")
+
+        bifunde = {
+            'befundeWeitere': data[4],
+            'befundeRadiologie': data[5],
+            'befunde': data[6],
+            'befundeLabor': data[7]
+        }
 
         bapdb = BAPDBData(berteleug=berteleug, anamnesis=anamnesis, procedure=procedure, diagnostic=diagnostic,
                           bifunde=bifunde)
+        return bapdb
