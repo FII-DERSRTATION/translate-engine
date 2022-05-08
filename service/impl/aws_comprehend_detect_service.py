9e4682c4ee54148c6d9a283e5d7f5eda35cd280a
Cristian Danila <mdanila@cisco.com>
2022-05-08 16:24:32 +0300

    Fix language code extraction logic

--- service/impl/aws_comprehend_detect_service.py
+++ service/impl/aws_comprehend_detect_service.py
@@ -6,16 +6,17 @@ import boto3
 aws_service = 'comprehend'
 aws_region_name = 'eu-central-1'
 
+
 class AWSComprehendDetectService(LanguageDetectService):
 
     @staticmethod
     def detect(data: str) -> str:
-
         comprehend = boto3.client(
             aws_access_key_id='AKIAUCYMDOCRXMRNIOPV',
             aws_secret_access_key='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',
             service_name=aws_service,
             region_name=aws_region_name)
 
-        result = json.dumps(comprehend.detect_dominant_language(Text = data))
-        return result['Languages']['LanguageCode']
+        result = comprehend.detect_dominant_language(Text=data)
+        print(result)
+        return result['Languages'][0]['LanguageCode']
